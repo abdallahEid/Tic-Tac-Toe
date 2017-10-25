5561a8a3889ce327bec0ad70b57dbd05521553d5
AbdallahEid <abdallaheidm@gmail.com>
2017-10-25 12:33:48 +0200

    Modifies the auto layout

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 1 is O , 2 is X
+    var activePlayer = 1
 
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        
+        if activePlayer == 1 {
+            
+            sender.setImage(UIImage(named: "nought.png") , for:[])
+            
+            activePlayer = 2
+            
+        } else {
+            
+            sender.setImage(UIImage(named: "cross.png") , for:[])
+            
+            activePlayer = 1
+            
+            
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
